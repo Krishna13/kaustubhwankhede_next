707d6706071147fb23e2ff94adfa799ba1e061e3
krishna13 <krishnawankhede2020@gmail.com>
2021-09-16 18:50:59 +0530

    test_commit

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import {useState} from 'react'
 import Link from 'next/link'
-function Header() {
+function Header(){
     const [navbarOpen, setNavbarOpen] = useState(false);
     return (
         <>
